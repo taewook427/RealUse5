7980a1d9e95053f99358641d1669b79461f04d59
taewook427 <144579627+taewook427@users.noreply.github.com>
2024-03-15 14:00:02 +0900

    Add files via upload

---
+++ stdlib/kio/kio.go
@@ -0,0 +1,167 @@
+// test627 : stdlib5.kio
+
+package kio
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+)
+
+// wait t, print any
+func Print(v interface{}, t float64) {
+	time.Sleep(time.Duration(t*1000) * time.Millisecond)
+	fmt.Print(v)
+}
+
+// ask q, get str input
+func Input(q string) string {
+	fmt.Print(q)
+	temp, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+	if temp[len(temp)-1] == '\n' {
+		temp = temp[0 : len(temp)-1]
+	}
+	if temp[len(temp)-1] == '\r' {
+		temp = temp[0 : len(temp)-1]
+	}
+	return temp
+}
+
+// compare two []byte
+func Bequal(a []byte, b []byte) bool {
+	if len(a) == len(b) {
+		for i, r := range a {
+			if b[i] != r {
+				return false
+			}
+		}
+		return true
+	} else {
+		return false
+	}
+}
+
+// read stringed []byte ("8a9B" -> B)
+func Bread(raw string) ([]byte, error) {
+	raw = strings.ToLower(raw)
+	if len(raw)%2 != 0 {
+		return nil, errors.New("not even length")
+	}
+	table := "0123456789abcdef"
+	out := make([]byte, len(raw)/2)
+	var cmp byte
+	var ptr byte
+	for i := 0; i < len(raw)/2; i++ {
+		cmp = raw[2*i]
+		ptr = 0
+		for cmp != table[ptr] {
+			ptr = ptr + 1
+			if ptr == 16 {
+				return nil, errors.New("invalid character")
+			}
+		}
+		out[i] = 16 * ptr
+
+		cmp = raw[2*i+1]
+		ptr = 0
+		for cmp != table[ptr] {
+			ptr = ptr + 1
+			if ptr == 16 {
+				return nil, errors.New("invalid character")
+			}
+		}
+		out[i] = out[i] + ptr
+	}
+	return out, nil
+}
+
+// conv []byte to string (B -> "39a2")
+func Bprint(raw []byte) string {
+	table := "0123456789abcdef"
+	out := make([]byte, len(raw)*2)
+	for i, r := range raw {
+		out[2*i] = table[r/16]
+		out[2*i+1] = table[r%16]
+	}
+	return string(out)
+}
+
+// absolute path (folder : */, file : *)
+func Abs(path string) string {
+	path, _ = filepath.Abs(path)
+	path = strings.Replace(path, "\\", "/", -1)
+	file, _ := os.Open(path)
+	defer file.Close()
+	fileinfo, _ := file.Stat()
+	if fileinfo.IsDir() {
+		if path[len(path)-1] != '/' {
+			path = path + "/"
+		}
+	}
+	return path
+}
+
+// get file Size (-1 : not Exist)
+func Size(path string) int {
+	fileinfo, err := os.Stat(path)
+	if err == nil {
+		return int(fileinfo.Size())
+	} else {
+		return -1
+	}
+}
+
+// file io pointer, "r"/"w"/"a"/"x"
+func Open(path string, mode string) (*os.File, error) {
+	var out *os.File
+	var err error
+
+	switch mode {
+	case "r":
+		out, err = os.Open(path)
+	case "w":
+		out, err = os.Create(path)
+	case "a":
+		out, err = os.OpenFile(path, os.O_RDWR|os.O_APPEND|os.O_CREATE, 0666)
+	case "x":
+		out, err = os.OpenFile(path, os.O_RDWR|os.O_TRUNC|os.O_CREATE, 0755)
+	default:
+		err = errors.New("invalid mode")
+	}
+
+	return out, err
+}
+
+// read bytes (-1 is readall)
+func Read(f *os.File, size int) ([]byte, error) {
+	if size < 0 {
+		fileinfo, fserr := f.Stat()
+		if fserr == nil {
+			size = int(fileinfo.Size())
+		} else {
+			return nil, fserr
+		}
+	}
+	temp := make([]byte, size)
+	success, err := f.Read(temp)
+	if err == nil {
+		return temp, nil
+	} else {
+		temp = temp[0:success]
+		return temp, err
+	}
+}
+
+// write bytes, returns not written bytes (pointer of original!!)
+func Write(f *os.File, data []byte) ([]byte, error) {
+	success, err := f.Write(data)
+	if err == nil {
+		return nil, nil
+	} else {
+		return data[success:], err
+	}
+}
---
+++ stdlib/kio/readme.txt
@@ -0,0 +1,26 @@
+stdlib5.kio [win & linux] : 터미널/파일 입출력 공통 함수들.
+
+<go>
+func Print(any v, float t)
+# t초 대기 후 v 내용 출력.
+func Input(str q) -> str
+# 질문 q 출력 후 \r\n 제거한 텍스트 응답 반환.
+func Bequal(byte[] a, byte[] b) -> bool
+# 두 바이트 슬라이스가 동일한지 확인.
+func Bread(str raw) -> (byte[], error)
+# 문자열 표현된 바이트를 바이트 슬라이스로 변환. (ex. "8a9C" -> B)
+func Bprint(byte[] raw) -> str
+# 바이트 슬라이스를 문자열로 변환. (ex. B -> "0c17")
+func Abs(str path) -> str
+# 입력 경로를 표준경로로 변환. (슬래시 + 폴더 슬래시 종결 + 절대 경로)
+func Size(str path) -> int
+# 파일 크기를 반환. 존재하지 않는 파일인 경우 -1 반환.
+func Open(str path, str mode) -> (OSFILE*, error)
+# 파일 경로와 모드를 받아 운영체제 입출력 파일 포인터 반환.
+"r" : 읽기 전용, "w" : 덮어쓰기, "a" : 이어쓰기, "x" : 실행권한 있는 덮어쓰기.
+func Read(OSFILE* f, int size) -> (byte[], error)
+# 일정 크기만큼 파일 읽기. size가 음수라면 전체 읽기. 읽기 성공한 크기만을 자동으로 잘라 반환.
+func Write(OSFILE* f, byte[] data) -> (byte[], error)
+# 파일 쓰기. 쓰기 실패한 바이트만큼 자동으로 잘려 반환됨.
+
+많은 코드에서 공통적으로 쓰일 만한 기능들을 포장했습니다.
